613495cbd63bd4a174a162e1f58840a16bba74be
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-12-15 03:51:03 +0000

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Module for the third round interview question
+Minimum number of operations
+"""
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    i = 2
+
+    while i * i <= n:
+        while n % i == 0:
+            operations += i
+            n //= i
+        i += 1
+
+    if n > 1:
+        operations += n
+
+    return operations
