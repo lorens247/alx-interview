b414e8c286fc1389418634d441543b78e4b05dc0
Lawrence Ikhidero <lawrence@ikhidero.com>
2024-05-06 05:51:59 +0100

    make change module

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Making Change Algorithm
+'''
+
+
+def makeChange(coins, total):
+    '''
+    Determine the fewest number of coins needed to meet a given amount total
+    from a pile of coins of different values...
+    '''
+
+    if total <= 0:
+        return 0
+
+    else:
+        dp = sorted(coins)
+        dp.reverse()
+        dp_count = 0
+        for i in dp:
+            while (total >= i):
+                dp_count += 1
+                total -= i
+        if total == 0:
+            return dp_count
+        return -1
+
+    
